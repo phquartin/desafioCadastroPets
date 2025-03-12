748842dd9e6f85df65db97bba56963c765135d3a
phquartin <phquartinmatos@sempreceub.com>
2025-03-11 23:36:56 -0300

    style: Modificado o SOUT do catch, apenas para ser mais visivel o erro.

---
+++ src/projeto/controller/ChangeController.java
@@ -0,0 +1,93 @@
+package projeto.controller;
+
+import projeto.repository.Repository;
+import projeto.service.CriteriaService;
+import projeto.service.PetService;
+
+import static projeto.util.Util.imprimirPets;
+import static projeto.controller.PetController.sc;
+
+public class ChangeController {
+    public void alterarPet() {
+        CriteriaController criteriaController = new CriteriaController();
+        String[][] petsComCriterios = new CriteriaService().buscarCriterios(criteriaController.definirCriterios());
+        if (petsComCriterios == null) {
+            return;
+        }
+        imprimirPets(petsComCriterios);
+
+        PetService tratamento = new PetService();
+        Repository repository = new Repository();
+        while (true) {
+            System.out.print("Digite o numero do pet que voce deseja alterar: ");
+            try {
+                int indexPet = Integer.parseInt(sc.nextLine()) - 1;
+                imprimirPets(petsComCriterios[indexPet]);
+                System.out.println("Oque voce deseja alterar:");
+                System.out.print("""
+                        1 - Nome/Sobrenome
+                        2 - Idade
+                        3 - Peso
+                        4 - Raca
+                        5 - Endereco
+                        0 - Cancelar
+                        :\s""");
+                int opcao = Integer.parseInt(sc.nextLine());
+                String alterar = "";
+                int indexAlterar = -1;
+                switch (opcao) {
+                    case 0:
+                        return;
+                    case 1:
+                        System.out.print("Digite o novo nome e sobrenome: ");
+                        String nome = sc.nextLine();
+                        alterar = tratamento.tratarNome(nome);
+                        indexAlterar = 0;
+                        break;
+                    case 2:
+                        System.out.print("Digite a nova idade: ");
+                        String stringIdade = sc.nextLine();
+                        double idade = tratamento.tratarIdade(stringIdade);
+                        alterar = Double.toString(idade);
+                        indexAlterar = 4;
+                        break;
+                    case 3:
+                        System.out.print("Digite o novo peso: ");
+                        String stringPeso = sc.nextLine();
+                        double peso = tratamento.tratarPeso(stringPeso);
+                        alterar = Double.toString(peso);
+                        indexAlterar = 5;
+                        break;
+                    case 4:
+                        System.out.print("Digite a nova raca: ");
+                        String raca = sc.nextLine();
+                        alterar = tratamento.tratarRaca(raca);
+                        indexAlterar = 6;
+                        break;
+                    case 5:
+                        System.out.print("Digite o endereco: ");
+                        System.out.print("Qual a cidade?: ");
+                        String cidade = sc.nextLine();
+                        System.out.print("Qual a rua?: ");
+                        String rua = sc.nextLine();
+                        System.out.print("Qual numero da casa/apartamento?: ");
+                        String numeroCasa = sc.nextLine();
+                        String[] enderecoArray = tratamento.tratarEndereco(cidade, rua, numeroCasa);
+                        alterar = enderecoArray[0] + ", " + enderecoArray[1] + ", " + enderecoArray[2];
+                        indexAlterar = 3;
+                        break;
+                    default:
+                        System.out.println("Numero invalido!");
+                        break;
+                }
+                repository.alterarPet(petsComCriterios[indexPet], alterar, indexAlterar);
+            } catch (NumberFormatException e) {
+                System.out.println("Digite um numero!");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Erro ao tentar alterar um pet!");
+            }
+            break;
+        }
+    }
+}
